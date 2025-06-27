4e2ba556f662713742d3e0891958e9d33dae343d
Yazhen Li <203456221+Yazhen-L@users.noreply.github.com>
2025-06-26 21:02:40 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,10 +3,5 @@
 apt-get update
 apt-get install -y build-essential python3-dev
 
-echo "Python version: $(python --version)"
-echo "Pip version: $(pip --version)"
-
 pip install --upgrade pip
 pip install -r requirements.txt
-
-pip list
