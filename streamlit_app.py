2067661d3ee09557f9b17713a58e26d06f7b6e25
Yazhen Li <203456221+Yazhen-L@users.noreply.github.com>
2025-06-27 10:08:53 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -916,14 +916,15 @@ elif page == "Prediction 📣":
             
             st.write("### ⚡️ Compare Top 3 Regressors with PyCaret")
 
-            # Get the DASshub Token
-            os.environ["DAGSHUB_TOKEN"] = "4d1d9fa89a353908874d7a4675b349668f941c66"
-            os.environ["DAGSHUB_USERNAME"] = "Yazhen-L"
-            DAGSHUB_TOKEN = os.getenv("DAGSHUB_TOKEN", "4d1d9fa89a353908874d7a4675b349668f941c66")
-            TokenStorage().add_token(host="https://dagshub.com", token=DAGSHUB_TOKEN)
+            os.environ["DAGSHUB_QUIET"] = "1"
+            try:
+                DAGSHUB_TOKEN = st.secrets["DAGSHUB_TOKEN"]
+            except KeyError:
+                st.error("DAGSHUB_TOKEN has not been set up yet，please check Streamlit Secrets")
+                st.stop()
 
             # DAGsHub MLflow Integration
-            dagshub.init(repo_owner='Yazhen-L', repo_name='First-Repo', mlflow=True)
+            dagshub.init(repo_owner='Yazhen-L', repo_name='First-Repo', mlflow=True, token=DAGSHUB_TOKEN)
 
             if "pycaret_triggered" not in st.session_state:
                 st.session_state["pycaret_triggered"] = False
