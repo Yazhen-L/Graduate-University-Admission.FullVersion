d641c1a5f70a2ffc40bfb4574994f77eae6ed9c5
Yazhen Li <203456221+Yazhen-L@users.noreply.github.com>
2025-06-27 01:25:53 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,7 @@ apt-get update
 apt-get install -y build-essential python3-dev
 
 pip install --upgrade pip
+pip install setuptools wheel
+
+pip install numpy==1.26.4
 pip install -r requirements.txt
